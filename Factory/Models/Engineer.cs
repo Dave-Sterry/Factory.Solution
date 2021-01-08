99b193cf66e0bade30845f6c67f2c6027ee5442d
David Sterry <sterry.david@gmail.com>
2021-01-08 08:53:42 -0800

    add classes to models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,6 +4,14 @@ namespace Factory.Models
 {
     public class Engineer
     {
-        
+      public Engineer()
+      {
+        this.JoinEntries = new HashSet<MachineEngineer>();
+      }
+
+      public int EngineerId { get; set; }
+      public string EngineerName { get; set; }
+
+      public ICollection<MachineEngineer> JoinEntries { get; }
     }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,13 @@ namespace Factory.Models
 {
     public class Machine
     {
-      
+      public Machine()
+      {
+        this.JoinEntries = new HashSet<MachineEngineer>();
+      }
+
+      public int MachineId { get; set; }
+      public string MachineName{ get; set; }
+      public virtual ICollection<MachineEngineer> JoinEntries { get; set; }
     }
 }
