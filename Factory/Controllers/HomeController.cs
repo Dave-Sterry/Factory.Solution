92c58c77136133fb5d7103b3a10ae82d5430d923
David Sterry <sterry.david@gmail.com>
2021-01-08 16:02:39 -0800

    add jumbtron and cards to splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,3 +1,7 @@
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Factory.Controllers
@@ -5,10 +9,23 @@ namespace Factory.Controllers
     public class HomeController : Controller
     {
 
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        Dictionary<object, object> model = new Dictionary<object, object>();
+        List<Engineer> engineers = _db.Engineers.ToList();
+        List<Machine> machines = _db.Machines.ToList();
+        model.Add("engineers", engineers);
+        model.Add("machines", machines);
+        return View(model);
       }
+      
     }
 }
